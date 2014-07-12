526eec1eacf4f319b1c03fc0620cc2b5eefe223b
Clive Norman <hi@normansolutions.co.uk>
2014-07-12 14:05:33 +0100

    Initial Load

---
+++ App_Code/CacheBust.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Caching;
+using System.Web.Hosting;
+
+/// <summary>
+/// Summary description for Cache
+/// </summary>
+public class CacheBust
+{
+
+    public static string FingerPrint(string rootRelativePath, string cdnPath = "")
+    {
+        if (!string.IsNullOrEmpty(cdnPath) && !HttpContext.Current.IsDebuggingEnabled)
+        {
+            return cdnPath;
+        }
+
+        if (HttpRuntime.Cache[rootRelativePath] == null)
+        {
+            string relative = VirtualPathUtility.ToAbsolute("~" + rootRelativePath);
+            string absolute = HostingEnvironment.MapPath(relative);
+
+            if (!File.Exists(absolute))
+            {
+                throw new FileNotFoundException("File not found", absolute);
+            }
+
+            DateTime date = File.GetLastWriteTime(absolute);
+            int index = relative.LastIndexOf('/');
+
+            string result = relative.Insert(index, "/v-" + date.Ticks);
+
+            HttpRuntime.Cache.Insert(rootRelativePath, result, new CacheDependency(absolute));
+        }
+
+        return HttpRuntime.Cache[rootRelativePath] as string;
+    }
+}
